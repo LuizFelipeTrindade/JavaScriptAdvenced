b381f72cb7bd7768b42b69509fec401bd1027cd8
Luiz Felipe <97800827+LuizFelipeTrindade@users.noreply.github.com>
2022-03-24 18:19:08 -0300

    commit

---
+++ variaveis/index.js
@@ -0,0 +1,18 @@
+//ECMA
+
+//Declarando uma variavel
+//var,let e const qual usar?
+
+let minhaVariavel; //undefined
+let outraVariavel; //undefined
+
+minhaVariavel = "olá";
+outraVariavel = 100;
+
+//var escopo da função
+//let e const escopo de bloco
+
+var a = 10;
+
+var b = 10;
+var c = 10;
