f8b8781511d4c9ef435e8050dc1b488ae4315bf9
Luiz Felipe <97800827+LuizFelipeTrindade@users.noreply.github.com>
2022-03-24 18:41:25 -0300

    commit

---
+++ TiposPrimitivos/index.js
@@ -0,0 +1,19 @@
+//QUASE TUDO EM JAVASCRIPT É UM OBJETO...
+
+//uma exceção são os tipos primitivos...
+
+//TIPOS PRIMITIVOS:
+//São 5: number,string,boolean,undefined,null
+
+let meuNumero =  10;
+let minhaString = "Olá Mundo";
+let meuBoolean = true;
+let meuNull = null;
+let meuUndefined = undefined;
+
+console.log('Meu número é: ', meuNumero);
+
+console.log('O tipo do número é: ', typeof meuNumero);
+console.log('O tipo da minha string é: ', typeof minhaString);
+console.log('O tipo do meu Null é: ', typeof meuNull);
+console.log('O tipo do meu Undefined é: ', typeof meuUndefined);
