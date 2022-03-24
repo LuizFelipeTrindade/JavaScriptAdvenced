3cd8a5a7d0b4cc26643a8432313e50bff5e80aeb
Luiz Felipe <97800827+LuizFelipeTrindade@users.noreply.github.com>
2022-03-24 19:19:25 -0300

    commit

---
+++ Funções1/function.js
@@ -0,0 +1,23 @@
+//função soma
+
+//Com declaração clássica
+function soma(num1, num2) {
+    const soma = num1 + num2;
+    return soma
+}
+
+console.log(soma(15, 15));
+
+const resultadoSoma = soma(15, 15);
+
+console.log(resultadoSoma);
+
+console.log(resultadoSoma + 10);
+
+//Com função Anônima
+
+function somaAnonima(vum1, vum2){
+    return vum1 + vum2
+}
+
+console.log(somaAnonima(5,6));
