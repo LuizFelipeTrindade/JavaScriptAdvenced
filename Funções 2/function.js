cbb63a780e979bcaf55101b361e1c99b76ab0506
Luiz Felipe <97800827+LuizFelipeTrindade@users.noreply.github.com>
2022-03-28 14:36:11 -0300

    commit

--- Funções 2/function.js
+++ Funções 2/function.js
@@ -1,4 +1,20 @@
   
 //outras formas de declarar funções
 
-//
+//função anônima(expressão da função):
+
+//function anonimaf = function(){
+//  return 'Sou o retorno da fnc'
+//}
+
+//console.log(anonimaf)
+
+//console.log(anonimaf())
+
+let anonimaf = function(){ //função anônima
+    return 'Sou o retorno da função'
+}
+
+console.log(anonimaf) //vai mostrar o valor da variavel
+
+console.log(anonimaf())//vai mostrar o valor da função
