61470a0c12a63daa1a593f28894acffdc3dd514b
Luiz Felipe <97800827+LuizFelipeTrindade@users.noreply.github.com>
2022-03-28 12:46:31 -0300

    commit

---
+++ Funções1/functions.js
@@ -0,0 +1,19 @@
+//ESTRUTURA INICIAL
+
+function nomeDaFuncao(a) {
+    //codigo aqui
+
+    //no final retorna explicitamente ou implicitamente
+}
+
+//EXEMPLO:
+
+let variavel = "This is string will be replaced by the return of the next function"
+
+function  funcReturnUndefined(){
+    let num1 = 10, num2 = 10, soma = num1 + num2;
+}
+
+variavel = funcReturnUndefined()
+
+ 
