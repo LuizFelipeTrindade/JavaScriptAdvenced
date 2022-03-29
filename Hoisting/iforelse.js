4b8e5cc56016f97f2915736ff9a49f910d95fad4
Luiz Felipe <97800827+LuizFelipeTrindade@users.noreply.github.com>
2022-03-29 16:46:02 -0300

    hosting

---
+++ Hoisting/iforelse.js
@@ -0,0 +1,19 @@
+//Hosting(elevação)
+
+// foo = 2;
+
+// console.log(foo);
+
+// var foo;
+
+hoisted();
+
+function hoisted(){
+    console.log("foo");
+}
+
+
+var x = 10;
+console.log(x, y);
+var y = 7;
+
