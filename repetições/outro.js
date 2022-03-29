0d1007d9c9ddeb01616672adc8184b3100539a2d
Luiz Felipe <97800827+LuizFelipeTrindade@users.noreply.github.com>
2022-03-29 17:13:06 -0300

    commit

---
+++ repetições/outro.js
@@ -0,0 +1,15 @@
+//repetições seguidas
+
+const numero = 1;
+
+if(numero === 1) {
+    console.log('é igual a 1');
+}
+
+if(numero === 2) {
+    console.log('É igual a 2');
+}
+
+if(numero === 3) {
+    console.log('É igual a 3');
+}
