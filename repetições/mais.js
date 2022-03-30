6b44682ca22f714bc833583a214b708d2240e09b
Luiz Felipe <97800827+LuizFelipeTrindade@users.noreply.github.com>
2022-03-30 18:29:21 -0300

    commit

--- repetições/mais.js
+++ repetições/mais.js
@@ -13,3 +13,23 @@ if((string1 === "Javaa") || (string2 === "JavaScript")){
 }else{
     console.log('Expressão if foi suprida com um valor falso');
 }
+
+const somaoumultiplica = (valor) => {
+    return valor >= 10? valor+valor : valor*valor
+}
+
+console.log('Menores que 10: ')
+console.log(somaoumultiplica (1))
+console.log(somaoumultiplica (2))
+console.log(somaoumultiplica (3))
+console.log(somaoumultiplica (6))
+console.log(somaoumultiplica (9))
+
+console.log('maiores ou iguais a 10: ')
+console.log(somaoumultiplica (10))
+console.log(somaoumultiplica (11))
+console.log(somaoumultiplica (20))
+console.log(somaoumultiplica (50))
+console.log(somaoumultiplica (100))
+
+
---
+++ repetições/switch.js
@@ -0,0 +1,19 @@
+const item = 1;
+
+switch(item) {
+    case 1: console.log('Item 1 - Setor de Brinquedo')
+    break
+    case 2: console.log('Item 2 - Setor de maquiagem')
+    break
+    case 3: console.log('Item 3 - Setor de Calçados')
+    break
+    case 4: console.log('Item 4 - Setor de eletrodomésticos')
+    break
+    case 5:
+    case 6:
+    case 7: console.log('Item 7 - Setor de vestuário')
+    break
+    case 8: console.log('Item 8 - Setor de utencílios')
+    break
+    default: console.log('Valor informado é invalido')
+}
