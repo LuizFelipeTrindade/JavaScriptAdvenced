3e62079663782c1ac362ac35a4bbcf594a45a3f3
Luiz Felipe <97800827+LuizFelipeTrindade@users.noreply.github.com>
2022-03-30 17:53:36 -0300

    commit

---
+++ repetições/mais.js
@@ -0,0 +1,15 @@
+const numero1 = 10, numero2 = 20;
+
+if((numero1 === 10) && (numero2 === 20)){
+    console.log('A expressão If foi sugerida com valor verdadeiro');
+}else {
+    console.log('Expressão if foi suprida com valor falso');
+}
+
+const string1 = "Java", string2 =  "Script";
+
+if((string1 === "Javaa") || (string2 === "JavaScript")){
+    console.log('Expressão if foi suprida com um valor verdadeiro');
+}else{
+    console.log('Expressão if foi suprida com um valor falso');
+}
