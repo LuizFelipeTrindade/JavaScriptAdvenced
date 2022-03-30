e463f16e421886c90c5ceae2090a559ef690c117
Luiz Felipe <97800827+LuizFelipeTrindade@users.noreply.github.com>
2022-03-30 19:23:49 -0300

    commit

---
+++ repetições/repetições1.js
@@ -0,0 +1,18 @@
+let c = 1
+
+do {
+    console.log(`Eu sou a ${c}`)
+    c++
+}while(c < 6)
+
+let str = "Sou uma máquina de matar krllll"
+let count = 0;
+
+do {
+    console.log(str[count])
+    count++
+}while(count < str.length)
+
+
+
+
