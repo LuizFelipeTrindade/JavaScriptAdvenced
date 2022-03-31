f47897ea99aee7cf981e6a285436f4ab0ad4d79c
Luiz Felipe <97800827+LuizFelipeTrindade@users.noreply.github.com>
2022-03-31 20:49:16 -0300

    commit

---
+++ repetições2/app.js
@@ -0,0 +1,38 @@
+ let c = 1
+
+ while(c < 100){
+    console.log(`O sexo vai começar em:${c}`)
+    c++
+}
+
+ let str = "Eu gosto é de sexo bixo"
+
+let con = 0
+ while(con < str.length){
+    console.log(str[con])
+    con++
+}
+
+
+let num = 1
+
+while(num < 10){
+    console.log(num)
+    num+=2
+}
+
+let numb = 1
+
+while(numb <= 20){
+    if(numb % 4 === 0){
+        console.log(numb)
+    }
+    numb++
+}
+
+let numero = 100
+
+while(numero <= 150){
+    console.log(num + 1);
+    num++
+}
