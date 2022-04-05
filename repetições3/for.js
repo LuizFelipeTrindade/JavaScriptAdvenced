dbedcd2d303b8da3cc10a514cc7d68270c4e5b36
Luiz Felipe <97800827+LuizFelipeTrindade@users.noreply.github.com>
2022-04-04 21:33:12 -0300

    for

---
+++ repetições3/for.js
@@ -0,0 +1,18 @@
+let str = "hello"
+
+for (let i = 0; let < str.length; i++){
+    console.log(str[i])
+}
+
+for (let c = -10; c <= 29, c++){
+    console.log(c)
+}
+
+//Pares
+
+for (let a = 1; a <= 10; a++){
+    if(a % 2 === 0){
+        console.log(a)
+    }
+}
+
