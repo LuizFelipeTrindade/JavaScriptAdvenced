1bb96a7674f4ba04ebcca47bec15c2585ebc60cb
Luiz Felipe <97800827+LuizFelipeTrindade@users.noreply.github.com>
2022-04-05 15:55:20 -0300

    objeto

--- objetos/objetos.js
+++ objetos/objetos.js
@@ -1,1 +1,18 @@
-const objetoVazio = {}
+//const objetoVazio = {}
+
+const dotNotation = {
+    prop1: "Sou uma propriedade que será acessada usando dot notation",
+    metodo: function(){
+        return "Sou um retorno de um método, ou seja, sou uma função dentro de um objeto"
+    }
+}
+
+dotNotation.prop2 = "Sou outra propriedade"
+dotNotation.metodo2 = () => "Sou o retorno do novo metodo"
+
+
+console.log(dotNotation.prop1)
+console.log(dotNotation.metodo())
+console.log(dotNotation.prop2)
+console.log(dotNotation.metodo2())
+
