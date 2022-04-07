79ad4bbe9b5004560001dee176c6c573193a9d57
Luiz Felipe <97800827+LuizFelipeTrindade@users.noreply.github.com>
2022-04-06 22:24:08 -0300

    commit

--- arrays/arrays.js
+++ arrays/arrays.js
@@ -9,3 +9,16 @@ let frutasUsandoNew = new Array('Maça', 'Banana')//declaração com argumento
 let arrTamanho = new Array(10)//dando o numero de posições como argumentos  
 
 console.log(frutasUsandoNew);
+
+//Array Denso
+const arrDenso = [1, 2, 3, 4, 5, 6];
+
+//Array esparso
+const arrEsparso = [1,,,3];
+
+arrEsparso[10] = 10;
+
+console.log(arrEsparso)
+console.log(arrDenso)
+
+
