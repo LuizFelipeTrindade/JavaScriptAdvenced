8fb7cafcb2fe82fcf05a2b2f6fb950b6a35b33c5
Luiz Felipe <97800827+LuizFelipeTrindade@users.noreply.github.com>
2022-04-06 21:54:37 -0300

    array

---
+++ arrays/arrays.js
@@ -0,0 +1,11 @@
+let frutas = ["Maça", "Banana", 5];
+
+console.log(frutas.length);
+
+//ou
+
+let frutasUsandoNew = new Array('Maça', 'Banana')//declaração com argumento
+
+let arrTamanho = new Array(10)//dando o numero de posições como argumentos  
+
+console.log(frutasUsandoNew);
