d5646d84c647c3786b7795cd88a75d9476acf385
Luiz Felipe <97800827+LuizFelipeTrindade@users.noreply.github.com>
2022-04-12 18:47:01 -0300

    commit

---
+++ Debugger/Debbuger.js
@@ -0,0 +1,29 @@
+let meuCarro = {
+    cor: "vermelho",
+    marca: "onix",
+    ano: "2004",
+    motor: null
+}
+
+console.log(meuCarro);
+
+let TestandoFunctionemObj = {
+    Carro: "HB20",
+    Cor: "Cinza",
+    Estado: "0KM",
+    f: function Carros(){
+        if("HB20"){
+            console.log('Quero esse')
+        }else{
+            console.log('Não quero esse')
+        }
+    }
+}
+
+console.log(TestandoFunctionemObj);
+
+console.log(TestandoFunctionemObj.f());
+
+
+
+
