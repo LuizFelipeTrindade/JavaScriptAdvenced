36ae296aee9051f7392d37a9d714135703bdfccb
Luiz Felipe <97800827+LuizFelipeTrindade@users.noreply.github.com>
2022-04-13 14:30:46 -0300

    Wrapper
    Wrapper function

--- Debugger/Debbuger.js
+++ Debugger/Debbuger.js
@@ -1,18 +1,18 @@
-let meuCarro = {
-    cor: "vermelho",
-    marca: "onix",
-    ano: "2004",
-    motor: null
-}
+// let meuCarro = {
+//     cor: "vermelho",
+//     marca: "onix",
+//     ano: "2004",
+//     motor: null
+// }
 
-console.log(meuCarro);
+//console.log(meuCarro);
 
 let TestandoFunctionemObj = {
     Carro: "HB20",
     Cor: "Cinza",
     Estado: "0KM",
     f: function Carros(){
-        if("HB20"){
+        if("HB20" && "Cinza"){
             console.log('Quero esse')
         }else{
             console.log('Não quero esse')
@@ -20,10 +20,29 @@ let TestandoFunctionemObj = {
     }
 }
 
+TestandoFunctionemObj.Cor = "Black";
+
 console.log(TestandoFunctionemObj);
 
 console.log(TestandoFunctionemObj.f());
 
+
+const str1 = "Brazinial Dev";
+
+console.log(str1)
+
+const funcaoLegal = (str) => {
+    const retorno = str1 + " é legal";
+    return retorno;
+}
+
+const retornoFuncaoLegal = funcaoLegal(str1)
+
+if(str1.length > 10) {
+    console.log(retornoFuncaoLegal)
+}else {
+    console.log("Não deu pra executar funcaoLegal")
+}
 
 
 
---
+++ Wrapper/wrapper.js
@@ -0,0 +1,20 @@
+// const str1 = String("Brazilian Dev");
+// const str2 = new String("Brazilian Dev2"); //Objeto
+// console.log(`str1 ${str1} str2 ${str2}`);
+
+// console.log(`str1 dividida = ${str1.split('')}
+// str2 dividida = ${str2.split('')}`);
+
+// const str2Convertido = str2.valueOf();
+// console.log(typeof str2Convertido);
+
+
+const num1 = Number('1');
+const num2 = new Number('2');
+
+console.log(`num1 ${num1} num2 ${num2}`);
+
+console.log(`Num1 com 2 casas décimais ${num1.toFixed(2)} 
+num 2 com 3 casas décimais ${num2.toFixed(3)}`)
+
+console.log(typeof num1)
