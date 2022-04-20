94cd43feae4652ff077eb220ca104b42d5bba54a
Luiz Felipe <97800827+LuizFelipeTrindade@users.noreply.github.com>
2022-04-20 16:13:18 -0300

    commit
    retorn the pratic programation

--- arrays/marray.js
+++ arrays/marray.js
@@ -1,3 +1,16 @@
 const meuArrayLiteral = [1,2,3] //Forma Literal
 
 const MeuArrayConstrutorInstance = new Array(1, 2, 3);
+
+const arrow = {
+    prop1: "Retornador",
+    f: function somador(){
+        return function somar(n1, n2){
+            return n1 + n2;
+        }
+    }
+}
+console.log(arrow(5, 2));
+console.log(arrow);
+
+
