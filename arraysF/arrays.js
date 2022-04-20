99a2d822727a3bdadb2135a539c7fc18d6695aff
Luiz Felipe <97800827+LuizFelipeTrindade@users.noreply.github.com>
2022-04-20 16:56:05 -0300

    Manipulando Arrays

---
+++ arraysF/arrays.js
@@ -0,0 +1,17 @@
+//Remover elementos em qualquer lugar
+
+const arrRemoveAnyPoint = [1,2,5,3];
+
+arrRemoveAnyPoint.splice(2/*Numero do índice*/,1/*Quantas deleções vou fazer */);
+
+console.log(arrRemoveAnyPoint);
+
+//Prencher arrays com valores 
+
+const arrPrenchido = new Array(10);
+
+arrPrenchido.fill(5); //Adiciona valores a um array vazio
+
+console.log(arrPrenchido);
+
+
