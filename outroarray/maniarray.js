091e5c42138ee3eab02fba3b7278196b4b5bfee2
Luiz Felipe <97800827+LuizFelipeTrindade@users.noreply.github.com>
2022-04-20 17:08:12 -0300

    commit

---
+++ outroarray/maniarray.js
@@ -0,0 +1,18 @@
+//Craindo Um subArray
+
+const arrSubArray = ["Luiz Felipe", "Developer", "Python", "Java", "Node"];
+
+const Subarray = arrSubArray.slice(2); // "Fatiar Array"
+
+console.log(Subarray);
+
+
+//Ordenando um Array 
+
+const arrayOrdenado = [2, 3, 1, 10, 9, 4, 5, 9, 8, 0];
+
+arrayOrdenado.sort((a,b) => a - b);
+
+console.log(arrayOrdenado);
+
+
