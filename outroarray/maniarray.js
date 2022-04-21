05e73f465240fe2943157d50abac54fcaf908057
Luiz Felipe <97800827+LuizFelipeTrindade@users.noreply.github.com>
2022-04-21 00:05:13 -0300

    manipulação de arrays

--- outroarray/maniarray.js
+++ outroarray/maniarray.js
@@ -11,14 +11,26 @@ console.log(Subarray);
 
 const arrayOrdenado = [2, 3, 1, 10, 9, 4, 5, 9, 8, 0];
 
-arrayOrdenado.sort((a,b) => a - b);
+arrayOrdenado.sort((a,b) => a - b); //Do menor para o Maior
 
 console.log(arrayOrdenado);
 
+// Filter, Fund, Foreach, Map, Redunce
 
-const uluu = () => {
-    return "Pedro é lindo"
-}
+const arrBase = [
+    {a: 2, b: 2},
+    {a: 1, b: 2},
+    {a: 5, b: 2},
+    {a: 1, b: 2},
+    {a: 10, b: 2}
+]
 
-console.log(uluu());
+const filtered = arrBase.filter(el => el.a > 2);
 
+const find = arrBase.find(el => el.a == 10);
+
+const foraaa = arrBase.forEach(el => console.log(`a: ${el.a}, b ${el.b}`));
+
+const maped = arrBase.map(el => el.a += 1);
+
+const reduceeee = arrBase.reduce((acc, curr) => acc = acc + curr.a, 0);
