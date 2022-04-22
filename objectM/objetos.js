6e67d9d7ce9de20a50d4e27fa23221907e84295f
Luiz Felipe <97800827+LuizFelipeTrindade@users.noreply.github.com>
2022-04-22 14:54:25 -0300

    manipulando objetos

--- objectM/objetos.js
+++ objectM/objetos.js
@@ -1,8 +1,23 @@
-const destino = {a: 1, b: 2};
-const origem = {b: 4, c: 5};
+// const destino = {a: 1, b: 2};
+// const origem = {b: 4, c: 5};
 
-const destinoretornado = Object.assign(destino, origem); //Retona a mistura dos dois objects
+// const destinoretornado = Object.assign(destino, origem); //Retona a mistura dos dois objects
 
-console.log(destinoretornado, destino);
+// console.log(destinoretornado, destino);
+
+//Clonagem ed objetos
+
+const obj = {a: 1};
+const copia = Object.assign({}, obj);
+
+//Mesclando objeto
+
+const o1 = {a: 1};
+const o2 = {b: 2};
+const o3 = {c: 3};
+
+const obja = Object.assign({} ,o1, o2, o3); //É como se fosse uma concatenação
+
+console.log(obja, o1);
 
 
