49ee0ec873cf664aa066d4ec9d75f0bc648ec945
Luiz Felipe <97800827+LuizFelipeTrindade@users.noreply.github.com>
2022-04-23 00:52:33 -0300

    string

---
+++ mstring/string.js
@@ -0,0 +1,23 @@
+//Métodos depreciados que interferem diretamente no DOM
+
+String.prototype.anchor();
+String.prototype.big();
+String.prototype.blink();
+String.prototype.bold();
+String.prototype.fixed();
+String.prototype.fontcolor();
+String.prototype.fontsize();
+String.prototype.italics();
+String.prototype.link();
+String.prototype.small();
+String.prototype.sub();
+String.prototype.sup();
+
+//MENOS USADOS e ESPECÍFICOS 
+
+String.prototype.codePointAt()
+String.fromCodePoint()
+String.prototype.normalize()
+String.prototype.localeCompare()
+String.prototype.toLocaleLowerCase()
+String.prototype.toLocaleUpperCase()
